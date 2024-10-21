a1c88387f6810ae92fbd73d52f45d5da30433bd8
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-22 04:10:25 +0530

    Concept of Arrays.

---
+++ PassingInFunctions.java
@@ -0,0 +1,15 @@
+package Java_DSA;
+
+import java.util.Arrays;
+
+public class PassingInFunctions {
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 6, 12};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
+}
