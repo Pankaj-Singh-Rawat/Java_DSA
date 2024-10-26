6f75ee69941d14944ea37944ec7872a1f2f9f582
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-26 21:39:09 +0530

    question of max item in Array

---
+++ MaxItemArray.java
@@ -0,0 +1,31 @@
+package Java_DSA;
+
+public class MaxItemArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23, 9, 18};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr , 4, 5));
+
+    }
+//    imagine that array is not empty
+    static int max(int[] arr){
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal ;
+    }
+
+    //maximum number in a range inside array
+    static int maxRange(int[] arr , int start , int end) {
+        int maxVal = arr[start];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
