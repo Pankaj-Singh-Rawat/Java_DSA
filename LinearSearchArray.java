94cdc153e33173d43c90c0c0aca152a363746024
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-28 23:33:24 +0530

    Linear search algo

---
+++ LinearSearchArray.java
@@ -0,0 +1,23 @@
+package Java_DSA;
+
+import java.util.Scanner;
+
+public class LinearSearchArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = {19, 22, 13, 43, 5};
+
+//        find n in array
+        System.out.println("what number to search in array? : ");
+        int n = in.nextInt();
+        for (int i = 0; i < arr.length ; i++) {
+            if( n == arr[i] ){
+                System.out.println(n + " found at index " + i );
+                break;
+
+            }
+        }
+
+    }
+}
