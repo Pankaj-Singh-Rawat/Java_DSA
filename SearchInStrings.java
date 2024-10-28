7c761793da3c4e1d7c89595fe8b8694c736613a8
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-28 23:44:52 +0530

    Linear search algo for strings

---
+++ SearchInStrings.java
@@ -0,0 +1,37 @@
+package Java_DSA;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+
+        String name = "Pankaj";
+        char target = 'u';
+        System.out.println(search(name, target));
+        System.out.println(search2(name , target));
+
+    }
+
+    static boolean search2(String str, char target) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        for (char ch : str.toCharArray() ) {
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    static boolean search(String str, char target) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
