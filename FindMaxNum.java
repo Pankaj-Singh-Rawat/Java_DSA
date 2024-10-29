722accb4a4530cba21743a069d61dc51e481bcf1
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2024-10-29 23:17:29 +0530

    maximum number in an array.

---
+++ FindMaxNum.java
@@ -0,0 +1,19 @@
+package Java_DSA;
+
+public class FindMaxNum {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, -7, 3, 14, 28};
+        System.out.println(max(arr));
+    }
+
+    static int max( int[] arr ){
+        int max = arr[0] ;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+        }
+        return max;
+    }
+}
