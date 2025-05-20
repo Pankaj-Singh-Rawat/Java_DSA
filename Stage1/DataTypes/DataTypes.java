4b28a117ba6fb99f581b5c7397909288ee6a8e19
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-05-20 21:40:43 +0530

    Data types

---
+++ Stage1/DataTypes/DataTypes.java
@@ -0,0 +1,51 @@
+// Primitive Data Type: boolean, char, byte, short, int, long, float, and double.
+// -> are fast , stored in stack 
+
+// boolean: 1 byte
+// byte: 1 byte
+// char: 2 byte
+// short: 2 byte
+// int: 4 byte
+// float: 4 byte
+// long: 8 byte
+// double: 8 byte
+
+// Non-Primitive Data Types (Object Types):  String, Array, Class, Interface, etc.
+// -> are slow , stored in heap
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Primitive data types: 
+        boolean b1 = true;
+        boolean b2 = false;
+
+        byte a = 25;
+        byte t = -10;
+
+        char g = 'A';
+        char s = '$';
+
+        short num = 1000;
+        short temp = -200;
+
+        int p = 2000000;
+        int d = 150000000;
+
+        float pi = 3.14f;
+        float gravity = 9.81f;
+
+        long w = 7800000000L;
+        long l = 9460730472580800L;
+
+        double piFull = 3.141592653589793;
+        double an = 6.02214076e23;
+
+        // Non Primitive data types:
+        String n = "Geek1";
+        String m = "Hello, World!";
+        
+    }
+    
+}
